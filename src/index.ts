ef0f0a983a584da9dcee7b6abc0482e124657270
Piotr Adamczyk <xeno@xeno.yt>
2024-05-07 15:22:16 +0200

    Fix format

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { sql } from 'drizzle-orm'
-import { db, queryClient } from './db/client'
+import { db, queryClient } from './db/client.js'
 
 const result = await db.execute<{ one: number }>(sql`SELECT 1 as one`)
 console.log(result[0]?.one)
