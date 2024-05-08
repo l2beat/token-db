ea442932a85fe8149271da37f5bb2f06b659402d
Piotr Adamczyk <xeno@xeno.yt>
2024-05-08 19:06:00 +0200

    Remove drizzle leftovers

--- drizzle/0003_clever_supreme_intelligence.sql
+++
@@ -1,6 +0,0 @@
-ALTER TYPE "source" ADD VALUE 'axelar-gateway';--> statement-breakpoint
-ALTER TABLE "network_rpcs" ALTER COLUMN "network_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "network_rpcs" ALTER COLUMN "url" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "networks" ALTER COLUMN "chain_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "networks" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "networks" ADD COLUMN "axelar_gateway" char(42);
