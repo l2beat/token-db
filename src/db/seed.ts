347b235dc6c8ef65c55e1342cd83fed83a9b695a
Piotr Adamczyk <xeno@xeno.yt>
2024-05-09 15:56:19 +0200

    Seed axelar stuff

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -35,6 +35,54 @@ async function seed() {
       })),
   })
 
+  const axelarConsts = {
+    ethereum: {
+      axelarGatewayAddress: '0x4F4495243837681061C4743b74B3eEdf548D56A5',
+      axelarId: 'ethereum',
+    },
+    'arbitrum-one': {
+      axelarGatewayAddress: '0xe432150cce91c13a887f7D836923d5597adD8E31',
+      axelarId: 'arbitrum',
+    },
+    'optimistic-ethereum': {
+      axelarId: 'optimism',
+      axelarGateway: '0xe432150cce91c13a887f7D836923d5597adD8E31',
+    },
+    avalanche: {
+      axelarId: 'avalanche',
+      axelarGateway: '0x5029C0EFf6C34351a0CEc334542cDb22c7928f78',
+    },
+    base: {
+      axelarId: 'base',
+      axelarGateway: '0xe432150cce91c13a887f7D836923d5597adD8E31',
+    },
+    'polygon-pos': {
+      axelarId: 'polygon',
+      axelarGateway: '0x6f015F16De9fC8791b234eF68D486d2bF203FBA8',
+    },
+    celo: {
+      axelarId: 'celo',
+      axelarGateway: '0xe432150cce91c13a887f7D836923d5597adD8E31',
+    },
+    linea: {
+      axelarId: 'linea',
+      axelarGateway: '0xe432150cce91c13a887f7D836923d5597adD8E31',
+    },
+  } as const
+
+  await db.$transaction(
+    Object.entries(axelarConsts).map(([coingeckoId, consts]) =>
+      db.network.update({
+        where: {
+          coingeckoId,
+        },
+        data: {
+          ...consts,
+        },
+      }),
+    ),
+  )
+
   console.log(`Database seeded with ${networks.length} networks ✅`)
 }
 
